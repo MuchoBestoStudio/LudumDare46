d035b468faed3a8850807e2b123460b0d1ec60e4
Rémi Carreira <remi.carreira@muchobestostudio.fr>
2020-04-18 19:53:40 +0200

    [update] menu scene and import TextMeshPro

---
+++ Ludum_Dare_46/Assets/Scripts/UI/Menu/MainMenuPanel.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MuchoBestoStudio.LudumDare.UI.Menu
+{
+	public class MainMenuPanel : MonoBehaviour
+	{
+		#region Variables
+
+		[SerializeField, Tooltip("")]
+		private string _gameplayScene = string.Empty;
+
+		[Header("Canvas")]
+		[SerializeField, Tooltip("")]
+		private	Canvas	_mainCanvas = null;
+		[SerializeField, Tooltip("")]
+		private Canvas	_playCanvas = null;
+		[SerializeField, Tooltip("")]
+		private	Canvas	_settingsCanvas = null;
+		[SerializeField, Tooltip("")]
+		private	Canvas	_creditsCanvas = null;
+
+		[Header("Buttons")]
+		[SerializeField, Tooltip("")]
+		private Button _play = null;
+		[SerializeField, Tooltip("")]
+		private Button _settings = null;
+		[SerializeField, Tooltip("")]
+		private	Button _credits = null;
+		[SerializeField, Tooltip("")]
+		private Button _quit = null;
+
+		#endregion
+
+		#region MonoBehaviour's Methods
+
+		private void OnEnable()
+		{
+			_play.onClick.AddListener(OnPlayButtonClicked);
+			_settings.onClick.AddListener(OnSettingsButtonClicked);
+			_credits.onClick.AddListener(OnCreditsButtonClicked);
+			_quit.onClick.AddListener(OnQuitButtonClicked);
+		}
+
+		private void OnDisable()
+		{
+			_play.onClick.RemoveListener(OnPlayButtonClicked);
+			_settings.onClick.RemoveListener(OnSettingsButtonClicked);
+			_credits.onClick.RemoveListener(OnCreditsButtonClicked);
+			_quit.onClick.RemoveListener(OnQuitButtonClicked);
+		}
+
+		#endregion
+
+		#region Buttons' callback
+
+		private void OnPlayButtonClicked()
+		{
+			_mainCanvas.enabled = false;
+			_playCanvas.enabled = true;
+
+			SceneManager.LoadScene(_gameplayScene, LoadSceneMode.Single);
+		}
+
+		private void OnSettingsButtonClicked()
+		{
+			_mainCanvas.enabled = false;
+			_settingsCanvas.enabled = true;
+		}
+
+		private void OnCreditsButtonClicked()
+		{
+			_mainCanvas.enabled = false;
+			_creditsCanvas.enabled = true;
+		}
+
+		private void OnQuitButtonClicked()
+		{
+			#if UNITY_EDITOR
+				UnityEditor.EditorApplication.isPlaying = false;
+			#else
+				Application.Quit();
+			#endif
+		}
+
+		#endregion
+	}
+}
---
+++ Ludum_Dare_46/Assets/Scripts/UI/Menu/SettingsPanel.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MuchoBestoStudio.LudumDare.UI.Menu
+{
+	public class SettingsPanel : MonoBehaviour
+	{
+		#region Variables
+
+		[Header("Canvas")]
+		[SerializeField, Tooltip("")]
+		private Canvas _settingsCanvas = null;
+		[SerializeField, Tooltip("")]
+		private Canvas _mainMenuCanvas = null;
+
+		[Header("Buttons")]
+		[SerializeField, Tooltip("")]
+		private Button _back = null;
+
+		#endregion
+
+		#region MonoBehaviour's Methods
+
+		private void OnEnable()
+		{
+			_back.onClick.AddListener(OnBackButtonClicked);
+		}
+
+		private void OnDisable()
+		{
+			_back.onClick.RemoveListener(OnBackButtonClicked);
+		}
+
+		#endregion
+
+		#region Buttons
+
+		private void OnBackButtonClicked()
+		{
+			_settingsCanvas.enabled = false;
+			_mainMenuCanvas.enabled = true;
+		}
+
+		#endregion
+	}
+}
