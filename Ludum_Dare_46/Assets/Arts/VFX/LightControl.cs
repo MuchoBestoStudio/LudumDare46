76f0e67999d01f2e90d51e36f09911a4be5938cf
Unknown <florian.boue@wanadoo.fr>
2020-04-18 13:26:21 +0200

    LA 3D !!!!!!!!

---
+++ Ludum_Dare_46/Assets/Arts/VFX/LightControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour {
+
+    float nRand = 0;
+    public float IntensityMin = 5;
+	public float IntensvityMax = 10;
+	
+	void Update ()
+    {
+        nRand = Random.RandomRange(IntensityMin, IntensvityMax);
+        this.transform.GetComponent<Light>().intensity = nRand;
+	}
+}
