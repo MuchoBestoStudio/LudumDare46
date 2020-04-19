3f06360bfecb4d96649ffb6dfe95135494a801bd
elymination <axel.dubillet@gmail.com>
2020-04-19 15:53:09 -0400

    Moved HUD elements and changed scaler to use reference resolution of 1920*1080. Updated UIManager to set the currency value in the game over UI scren.

--- Ludum_Dare_46/Assets/Scripts/UI/ViewModels/GameManagerViewModel.cs
+++ Ludum_Dare_46/Assets/Scripts/UI/ViewModels/GameManagerViewModel.cs
@@ -12,20 +12,41 @@ namespace MuchoBestoStudio.LudumDare.UI.ViewModels
         [SerializeField]
         private Text _timeText = null;
         [SerializeField]
-        private string _taggedString = null;
+        private Text _currencyText = null;
+        [SerializeField]
+        private string _timeTaggedString = null;
+        [SerializeField]
+        private string _currencyTaggedString = null;
+
+        private CurrencySystem _currencySystem = null;
 
         public void Start()
         {
+            _currencySystem = FindObjectOfType<CurrencySystem>();
             _gameManager = GameManager.Instance;
             UpdateTimeText();
             _gameManager.onTimeUpdated += UpdateTimeText;
+            if (_currencySystem)
+            {
+                _currencySystem.OnCurrencyChanged += UpdateCurrencyText;
+            }
         }
 
         private void UpdateTimeText()
         {
             string resultString;
-            resultString = StringTagReplacer.ReplaceTag(_taggedString, "[VALUE]", ((int)_gameManager.GameTime).ToString());
+            resultString = StringTagReplacer.ReplaceTag(_timeTaggedString, "[VALUE]", ((int)_gameManager.GameTime).ToString());
             _timeText.text = resultString;
+        }
+
+        private void UpdateCurrencyText(uint value)
+        {
+            if (_currencySystem)
+            {
+                string resultString;
+                resultString = StringTagReplacer.ReplaceTag(_currencyTaggedString, "[VALUE]", _currencySystem.Currency.ToString());
+                _currencyText.text = resultString;
+            }
         }
     }
 }
